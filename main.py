49c6127f93419651d6f5b3d1d28def12f2c926b7
Nhat Tan <nhattan3105.forwork@gmail.com>
2024-04-27 20:20:31 +0700

    multi thread and Qtimer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QMessageBox, QFileDialog
 from PyQt5.QtGui import QPixmap, QImage, QFont
 from sface import *
 import shutil
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer, QObject
 
 class LoadingScreen(QtWidgets.QWidget):
     def __init__(self):
@@ -61,7 +61,6 @@ class VideoLabel(QtWidgets.QLabel):
     def setPixmap(self, pixmap):
         super().setPixmap(pixmap)
         self.setScaledContents(True)
-
 class StreamThread(QThread):
     updatePixmap = pyqtSignal(QtGui.QPixmap)
 
@@ -69,11 +68,20 @@ class StreamThread(QThread):
         super(StreamThread, self).__init__(parent)
         self.url = url
         self.active = True
+        self.cap = cv2.VideoCapture(self.url)
+        self.timer = QTimer()
+        self.timer.moveToThread(self.thread())  # Đảm bảo QTimer được tạo trong luồng chính
+        self.timer.timeout.connect(self.read_frame)
 
     def run(self):
-        cap = cv2.VideoCapture(self.url)
-        while self.active:
-            ret, frame = cap.read()
+        QTimer.singleShot(0, self.start_timer)  # Khởi động QTimer trong luồng chính
+
+    def start_timer(self):
+        self.timer.start(90)  # Đọc khung hình mỗi 30 ms
+
+    def read_frame(self):
+        if self.active:
+            ret, frame = self.cap.read()
             if ret:
                 rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgb_image.shape
@@ -81,12 +89,11 @@ class StreamThread(QThread):
                 q_img = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
                 pixmap = QtGui.QPixmap.fromImage(q_img)
                 self.updatePixmap.emit(pixmap)
-            else:
-                break
-        cap.release()
 
     def stop(self):
         self.active = False
+        self.timer.stop()
+        self.cap.release()
 
 class CCTV(object):
     def setupUi(self, MainWindow):
